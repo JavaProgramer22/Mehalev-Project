7aac1588174f8f8851fe0794595d22199586838f
Jeries Zamel <Zamel.65@gmail.com>
2019-10-05 01:48:00 +0300

    navbar header footer

---
+++ app.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import AppRouter from "./src/router/Router";
+
+// ReactDOM.render(<AppRouter />, document.querySelector("#container"));
+import AppRouter from "./src/router/Router";
+import Loading from "./loading";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      });
+    }, 2000);
+  }
+
+  render() {
+    return this.state.isLoading ? <Loading /> : <AppRouter />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#container"));
